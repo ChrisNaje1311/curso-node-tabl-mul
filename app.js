271d7f8a202dff70a8ed8b0a922a90fc2d25b45a
Christian Najera <najera.govea@gmail.com>
2023-01-07 10:09:25 -0700

    Primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const { boolean } = require('yargs');
+const {crearArchivo} = require('./helpers/multiplicar')
+const argv= require('./config/yargs');
+const colors = require('colors');
+
+
+
+
+console.clear();
+
+
+crearArchivo(argv.b, argv.l,argv.h)
+.then(nombreArchivo => console.log(nombreArchivo.blue,'creado'))
+.catch(err => console.log(err)); 
+ 
